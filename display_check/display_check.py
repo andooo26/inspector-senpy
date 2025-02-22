4bd730e470af656a2b4532d7857ef66bba21d839
andooo26 <yota426513@gmail.com>
2025-02-22 12:13:11 +0900

    exeった

--- display_check/display_check.py
+++ display_check/display_check.py
@@ -1,10 +1,23 @@
 import tkinter as tk
+import sys
 
-def color():
+def changeColor():
     current_color = canvas.cget("bg")
-    new_color = 'black' if current_color == 'white' else 'white'
+    if current_color == 'black':
+        new_color = 'white'
+    elif current_color == 'white':
+        new_color = 'red'
+    elif current_color == 'red':
+        new_color = 'yellow'
+    elif current_color == 'yellow':
+        new_color = 'blue'
+    elif current_color == 'blue':
+        new_color = 'black' 
     canvas.config(bg=new_color)
-    root.after(2000, color)
+    root.after(2000, changeColor)
+
+def on_key_press(event):
+    root.quit()
 
 root = tk.Tk()
 root.title("sample")
@@ -19,6 +32,8 @@ frame.pack()
 canvas = tk.Canvas(frame, width=width, height=height, bg='black', highlightthickness=0, relief='ridge')
 canvas.pack()
 
-root.after(2000, color)
+root.bind("<KeyPress>", on_key_press)
+
+root.after(2000, changeColor)
 
 root.mainloop()
